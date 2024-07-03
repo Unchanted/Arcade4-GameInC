7c98d2f68f03a43ca5754f3ece875caec720c977
Unchanted <omkkdwivedi@gmail.com>
2024-07-03 10:04:26 +0530

    organizing and creating makefile

--- src/puzzle.c
+++ src/puzzle.c
@@ -3,15 +3,15 @@
 Square *** setUpPuzzle(int ** puzzle)
 {
     Square *** sudoku;
-    int i, j;
+    int i, j, x;
 
     sudoku = (Square***)malloc(sizeof(Square**)*9);
 
-    for (i = 0; i < 9; i++)
+    for (i = 0; i < SIZE_ROWS; i++)
     {
         sudoku[i] = (Square**)malloc(sizeof(Square*)*9);
 
-        for (j = 0; j < 9; j++)
+        for (j = 0; j < SIZE_COLUMNS; j++)
         {
             sudoku[i][j] = (Square*)malloc(sizeof(Square)*9);
 
@@ -19,19 +19,78 @@ Square *** setUpPuzzle(int ** puzzle)
 
             sudoku[i][j]->row = i;
             sudoku[i][j]->column = j;
+            sudoku[i][j]->solvable = 9;
 
+            for (x = 0; x < SIZE_ROWS; x++)
+            {
+                sudoku[i][j]->possible[x] = 0;
+            }
+
+        }
+    }
+
+    for (i = 0; i < SIZE_ROWS; i++)
+    {
+        for (j = 0; j < SIZE_COLUMNS; j++)
+        {
             if (sudoku[i][j]->number != 0)
             {
-                sudoku[i][j]->code = POSSIBLE;
-            }
-            else
-            {
-                sudoku[i][j]->code = 0x0;
+                sudoku[i][j]->solvable = 0;
+                updateSudoku(sudoku, i, j);
+                UNSOLVED--;
             }
         }
     }
 
+    return sudoku;
 
+}
+
+int updateSudoku(Square *** sudoku, int row, int column)
+{
+    int x;
+    int number = sudoku[row][column]->number;
+
+    for (x = 0; x < SIZE_ROWS; x++)
+    {
+        if (sudoku[x][column]->possible[number - 1] == 0)
+        {
+            sudoku[x][column]->solvable--;
+        }
+        sudoku[x][column]->possible[number -1] = 1;
+    }
+
+    for (x = 0; x < SIZE_COLUMNS; x++)
+    {
+        if (sudoku[row][x]->possible[number - 1] == 0)
+        {
+            sudoku[row][x]->solvable--;
+        }
+        sudoku[row][x]->possible[number -1] = 1;
+    }
+
+    return 1;
+
+}
+
+int checkPuzzle(Square *** sudoku)
+{
+    int i, j, x;
+
+    for (i = 0; i < SIZE_ROWS; i++)
+    {
+        for (j = 0; j < SIZE_COLUMNS; j++)
+        {
+            if (sudoku[i][j]->solvable == 1)
+            {
+                solveSquare(sudoku[i][j]);
+                updateSudoku(sudoku, i, j);
+            }
+
+        }
+
+    }
+    return 1;
 }
 
 int ** createPuzzle()
@@ -65,17 +124,18 @@ int ** createPuzzle()
     return puzzle;
 }
 
-void printPuzzle(int ** puzzle)
+void printPuzzle(Square *** puzzle)
 {
     int i, j;
 
     printf("-------------------------------\n");
-    for (i = 0; i < 9; i++)
+    for (i = 0; i < SIZE_ROWS; i++)
     {
         printf("|");
-        for (j = 0; j < 9; j++)
+
+        for (j = 0; j < SIZE_COLUMNS; j++)
         {
-            printf(" %d ", puzzle[i][j]);
+            printf(" %d ", puzzle[i][j]->number);
 
             if (((j + 1) % 3) == 0)
             {
